bebb4807e5aa6fc6cf0328c53b67e4350cc88aa0
curiousvishu <shivharevishal14@gmail.com>
2023-08-05 12:32:51 +0530

    added documentation

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+//Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 // connectDB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
@@ -29,24 +34,24 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
 //
 // routes
 app.get('/', (req, res) => {
-  res.send('jobs api')
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
+
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
-
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
